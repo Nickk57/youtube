5e13bdd94bbcbfd639a80ed05ddf48a766d0763e
nicolas guichard <guichard.nicolas57@gmail.com>
2023-02-06 16:10:33 +0100

    php

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" style="text/css" href="style/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <?php include('include/nav_bar.php') ?>
+    </header>
+    <main>
+    <div class="grid">
+    <div class="grid justify-content-center">
+        <div class="row justify-content-center">
+            <div class="col">
+                <img src="">
+                <h2>TEXT</h2>
+                <p>youtubeur<br>
+                nombre de vue ° date de public.</p>
+                </div>
+            </div>
+            <div class="col">
+                <img src="">
+                <h2>TEXT</h2>
+                <p>youtubeur<br>
+                    nombre de vue ° date de public.</p>
+            </div>
+            <div class="col">
+                <img src="">
+                <h2>TEXT</h2>
+                <p>youtubeur<br>
+                nombre de vue ° date de public.</p>
+            </div>
+        </div>
+    </div>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
